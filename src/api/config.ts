d2e9eb7cbd23e42524ca164db1c48d21a713b9bc
Lucas Oliveira <lucas.gracas@germinare.org.br>
2025-10-25 14:54:13 -0300

    feat: Configuring Firebase connection to integration

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: import.meta.env.DEV ?
+    "/api"
+    :
+    import.meta.env.VITE_SQL_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
